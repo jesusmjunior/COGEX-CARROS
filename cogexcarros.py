abd3dfc6437811b5e16ba4bde7d55e7344cd37f1
jesusmjunior <134287070+jesusmjunior@users.noreply.github.com>
2025-03-18 13:22:33 -0300

    Update cogexcarros.py

--- cogexcarros.py
+++ cogexcarros.py
@@ -113,6 +113,12 @@ def resumo_dados(dataframe):
         </div>
     """, unsafe_allow_html=True)
 
+elif aba_selecionada == 'Sinistro':
+    st.header("📄 Sinistros")
+    resumo_dados(df)
+    st.dataframe(df, use_container_width=True)
+    botao_download(df, "sinistros.csv")
+    exportar_pdf_html(df)
 else:
     df = sheets[aba_selecionada]
     st.write("### 📄 Registros")
